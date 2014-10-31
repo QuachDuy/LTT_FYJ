7f5498a9a0b838f0c5d2675690c5e730970eb6d9
ThoLH <tholh@lifetimetech.vn>
2014-10-31 11:41:18 +0700

    add document

---
+++ document/db/fyj_20141031.sql
@@ -0,0 +1,195 @@
+/*
+SQLyog Enterprise v10.42 
+MySQL - 5.5.16 : Database - fyj
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`fyj` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
+
+/*Table structure for table `tb_admins` */
+
+CREATE TABLE `tb_admins` (
+  `id` varchar(36) COLLATE utf8_unicode_ci NOT NULL,
+  `name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `gender` tinyint(1) NOT NULL,
+  `disactive_flag` tinyint(1) NOT NULL DEFAULT '0',
+  `creator` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `created` datetime NOT NULL,
+  `updater` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `updated` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Table structure for table `tb_careers` */
+
+CREATE TABLE `tb_careers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `disable_flag` tinyint(1) NOT NULL DEFAULT '0',
+  `creator` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `created` datetime NOT NULL,
+  `updater` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `updated` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Table structure for table `tb_conpanies` */
+
+CREATE TABLE `tb_conpanies` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `address` text COLLATE utf8_unicode_ci NOT NULL,
+  `description` text COLLATE utf8_unicode_ci,
+  `size_id` int(11) NOT NULL,
+  `disable_flag` tinyint(1) NOT NULL DEFAULT '0',
+  `creator` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `created` datetime NOT NULL,
+  `updater` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `updated` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Table structure for table `tb_conpany_media` */
+
+CREATE TABLE `tb_conpany_media` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `company_id` int(100) NOT NULL,
+  `path` text COLLATE utf8_unicode_ci NOT NULL,
+  `type` int(11) NOT NULL,
+  `disable_flag` tinyint(1) NOT NULL DEFAULT '0',
+  `creator` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `created` datetime NOT NULL,
+  `updater` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `updated` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Table structure for table `tb_employer_quota` */
+
+CREATE TABLE `tb_employer_quota` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `employer_id` varchar(36) COLLATE utf8_unicode_ci NOT NULL,
+  `priority` varchar(2) COLLATE utf8_unicode_ci NOT NULL COMMENT 'NO: normal; V1: VIP1; V2: VIP2',
+  `quota` int(11) NOT NULL,
+  `disable_flag` tinyint(1) NOT NULL DEFAULT '0',
+  `creator` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `created` datetime NOT NULL,
+  `updater` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `updated` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Table structure for table `tb_employers` */
+
+CREATE TABLE `tb_employers` (
+  `id` varchar(36) COLLATE utf8_unicode_ci NOT NULL,
+  `name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `company_id` int(11) DEFAULT NULL,
+  `disable_flag` tinyint(1) NOT NULL,
+  `creator` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `created` datetime NOT NULL,
+  `updater` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `updated` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Table structure for table `tb_jobs` */
+
+CREATE TABLE `tb_jobs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `career_id` int(11) NOT NULL,
+  `company_id` int(11) NOT NULL,
+  `location_id` int(11) NOT NULL,
+  `priority` varchar(2) COLLATE utf8_unicode_ci NOT NULL,
+  `description` text COLLATE utf8_unicode_ci NOT NULL,
+  `requirement` text COLLATE utf8_unicode_ci,
+  `contact` text COLLATE utf8_unicode_ci,
+  `start_date` datetime NOT NULL,
+  `end_date` datetime NOT NULL,
+  `disable_flag` tinyint(1) NOT NULL DEFAULT '0',
+  `creator` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `created` datetime NOT NULL,
+  `updater` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `updated` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Table structure for table `tb_locations` */
+
+CREATE TABLE `tb_locations` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `disable_flag` tinyint(1) NOT NULL DEFAULT '0',
+  `creator` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `created` datetime NOT NULL,
+  `updater` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `updated` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Table structure for table `tb_oders` */
+
+CREATE TABLE `tb_oders` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `company_id` varchar(36) COLLATE utf8_unicode_ci NOT NULL,
+  `packet_id` int(11) NOT NULL,
+  `disable_flag` tinyint(1) NOT NULL DEFAULT '0',
+  `creator` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `created` datetime NOT NULL,
+  `updater` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `updated` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Table structure for table `tb_packets` */
+
+CREATE TABLE `tb_packets` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `description` text COLLATE utf8_unicode_ci,
+  `priority` varchar(2) COLLATE utf8_unicode_ci NOT NULL,
+  `quota` int(11) NOT NULL,
+  `price` int(11) NOT NULL,
+  `disable_flag` tinyint(1) NOT NULL DEFAULT '0',
+  `creator` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `created` datetime NOT NULL,
+  `updater` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `updated` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Table structure for table `tb_users` */
+
+CREATE TABLE `tb_users` (
+  `id` varchar(36) COLLATE utf8_unicode_ci NOT NULL,
+  `name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `gender` tinyint(1) DEFAULT NULL,
+  `birthday` date DEFAULT NULL,
+  `phone` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `disable_flag` tinyint(1) NOT NULL,
+  `creator` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `created` datetime NOT NULL,
+  `updater` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `updated` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
